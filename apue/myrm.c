2115923f14f6eeaa795ae49080183c9e02e4c482
lixudong <lixudong0325@163.com>
2021-01-15 10:31:17 +0800

    myrm.c

--- apue/myrm.c
+++ apue/myrm.c
@@ -1,13 +1,46 @@
 #include "apue.h"
 #include <fcntl.h>
-
+#include <dirent.h>
+int rmfile(const char *path)
+{
+	DIR *dp;
+	struct stat filest;
+	struct dirent   *dirp;
+	int sum=0;
+	if(lstat(path,&filest)==0)
+	{
+		if(S_ISDIR(filest.st_mode))
+		{
+			
+			if((dp = opendir(path)) == NULL)
+                                err_sys("can't open %s",path);
+			chdir(path);
+			while((dirp = readdir(dp))!=NULL)
+                        {
+				if(strcmp(dirp->d_name,".")==0||strcmp(dirp->d_name,"..")==0)
+					continue;
+				sum+=rmfile(dirp->d_name);
+                        }
+			chdir("..");
+			closedir(dp);
+			rmdir(path);
+			printf("++++++%s\n",path);
+		}
+		else{
+			unlink(path);	
+			printf("------%s\n",path);
+			sum+=1;
+		}
+		return sum;
+	}
+	return 0;
+	
+}
 int main(int argc,char *argv[])
 {
-	if(open(argv[1],O_RDWR)<0)
-		err_sys("open error");
-	if(unlink(argv[1])<0)
-		err_sys("unlink error");
-	printf("file unlinked\n");
-	sleep(15);
-	printf("done\n");
+	
+        if(argc < 2)
+		err_quit("usage: myrm <filepath>");
+	printf("delete file sum:%d\n",rmfile(argv[1]));
+	
 }
